16f68edb03500d8f27be82ea8f73c7e6669bbd0e
thisisaaronland <aaron@mapzen.com>
2016-04-11 21:06:30 +0000

    add hooks to move config in to /etc/logstash/conf.d/

--- ubuntu/setup-logstash.sh
+++ ubuntu/setup-logstash.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+WHOAMI=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+WHEREAMI=`dirname $WHOAMI`
+ROOT=`dirname $WHEREAMI`
+
 # https://www.digitalocean.com/community/tutorials/how-to-install-elasticsearch-logstash-and-kibana-elk-stack-on-ubuntu-14-04
 
 sudo apt-get update
@@ -9,5 +13,26 @@ echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee
 
 sudo apt-get update
 sudo apt-get install logstash
+
+LOGSTASH="${ROOT}/logstash/"
+
+if [ ! -f ${LOGSTASH}/flamework-logstash.conf ]
+then
+    cp ${LOGSTASH}/flamework-logstash.conf.example ${LOGSTASH}/flamework-logstash.conf
+fi
+
+if [ -L /etc/logstash/conf.d/flamework-logstash.conf ]
+then
+    sudo rm /etc/logstash/conf.d/flamework-logstash.conf
+fi
+
+if [ -f /etc/logstash/conf.d/flamework-logstash.conf ]
+then
+    sudo mv /etc/logstash/conf.d/flamework-logstash.conf /etc/logstash/conf.d/flamework-logstash.conf.bak
+fi
+
+sudo ln -s ${LOGSTASH}/flamework-logstash.conf /etc/logstash/conf.d/flamework-logstash.conf
+
+sudo /etc/init.d/logstash restart
 
 exit 0
